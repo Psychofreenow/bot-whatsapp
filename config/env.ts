e8f43ce314c4a763c79941f3a7d1cd4eebd161fb
Franklin <fmarquez@visiion>
2026-02-28 12:17:07 -0300

    UPDATE: config env

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const env = {
   PORT: process.env.PORT || 3000,
   VERIFY_TOKEN: process.env.VERIFY_TOKEN || "",
@@ -7,4 +11,7 @@ export const env = {
   SENDER_PHONE_NUMBER_ID: process.env.SENDER_PHONE_NUMBER_ID || "",
   URL_BASE_FACEBOOK_GRAPH: process.env.URL_BASE_FACEBOOK_GRAPH || "",
   OPENAI_KEY: process.env.OPENAI_KEY || "",
+  DEEPSEEK_KEY: process.env.DEEPSEEK_KEY || "",
+  GEMINI_KEY: process.env.GEMINI_KEY || "",
+  DATABASE_URL: process.env.DATABASE_URL || "",
 };
